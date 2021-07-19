2162e83be35884296d851bfc44eb84d9b5dda4f0
mferrara625 <mferrara401@yahoo.com>
2021-07-19 14:44:34 -0400

    Two player connect four without scoring system

---
+++ src/com/company/Gameboard.java
@@ -0,0 +1,61 @@
+package com.company;
+
+public class Gameboard {
+    String a1 = "_";
+    String a2 = "_";
+    String a3 = "_";
+    String a4 = "_";
+    String a5 = "_";
+    String a6 = "_";
+    String b1 = "_";
+    String b2 = "_";
+    String b3 = "_";
+    String b4 = "_";
+    String b5 = "_";
+    String b6 = "_";
+    String c1 = "_";
+    String c2 = "_";
+    String c3 = "_";
+    String c4 = "_";
+    String c5 = "_";
+    String c6 = "_";
+    String d1 = "_";
+    String d2 = "_";
+    String d3 = "_";
+    String d4 = "_";
+    String d5 = "_";
+    String d6 = "_";
+    String e1 = "_";
+    String e2 = "_";
+    String e3 = "_";
+    String e4 = "_";
+    String e5 = "_";
+    String e6 = "_";
+    String f1 = "_";
+    String f2 = "_";
+    String f3 = "_";
+    String f4 = "_";
+    String f5 = "_";
+    String f6 = "_";
+    String g1 = "_";
+    String g2 = "_";
+    String g3 = "_";
+    String g4 = "_";
+    String g5 = "_";
+    String g6 = "_";
+
+    public Gameboard(){
+
+    }
+    public String toString(){
+        return "        _______________\n" +
+                "        |"+a1+"|"+b1+"|"+c1+"|"+d1+"|"+e1+"|"+f1+"|"+g1+"|\n" +
+                "        |"+a2+"|"+b2+"|"+c2+"|"+d2+"|"+e2+"|"+f2+"|"+g2+"|\n" +
+                "        |"+a3+"|"+b3+"|"+c3+"|"+d3+"|"+e3+"|"+f3+"|"+g3+"|\n" +
+                "        |"+a4+"|"+b4+"|"+c4+"|"+d4+"|"+e4+"|"+f4+"|"+g4+"|\n" +
+                "        |"+a5+"|"+b5+"|"+c5+"|"+d5+"|"+e5+"|"+f5+"|"+g5+"|\n" +
+                "        |"+a6+"|"+b6+"|"+c6+"|"+d6+"|"+e6+"|"+f6+"|"+g6+"|\n" +
+                "        |1|2|3|4|5|6|7|";
+    }
+
+}
